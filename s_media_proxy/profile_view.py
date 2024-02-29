afb108e5b0b04a6eeca0611d31c4833a4d7072d9
Dima <pythonprogrammer9999@gmail.com>
2024-02-29 15:52:28 +0300

    add permission for feature/profile

--- s_media_proxy/profile_view.py
+++ s_media_proxy/profile_view.py
@@ -1,6 +1,8 @@
 from rest_framework import viewsets
 from s_media_proxy.profile_serializer import UserProfileSerializer
 from s_media_proxy import models
+from s_media_proxy.permissions import UpdateOwnProfile
+from rest_framework.authentication import TokenAuthentication
 
 
 class UserProfileViewSet(viewsets.ModelViewSet):
@@ -8,4 +10,6 @@ class UserProfileViewSet(viewsets.ModelViewSet):
 
     serializer_class = UserProfileSerializer
     queryset = models.User.objects.all()
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (UpdateOwnProfile,)
 
